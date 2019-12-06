5ac1024624085d529b70f9cc70bf2b51c481e396
drohiro <rodrigorfsfr3@gmail.com>
2019-12-06 15:35:17 -0300

    tt

--- index.js
+++
@@ -1,5 +0,0 @@
-const app = require('./src/config/config-express')
-
-app.listen(3000, function(){
-    console.log('servidor em pé');
-});
--- src/config/config-express.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-app = express();
-
-const rotas = require('../app/routes/routes');
-rotas(app);
-
-module.exports = app;
